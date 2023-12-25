d4276691a54ab8068d6f909887d10f5f2d11bfa6
sslukess <lvss.dev@gmail.com>
2023-12-25 13:27:41 +0000

    unneeded import removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Footer } from './components/footer/Footer.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
