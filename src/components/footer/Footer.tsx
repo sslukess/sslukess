893c850658129dfcf4615b19e0384504f0c77ab4
sslukess <lvss.dev@gmail.com>
2023-12-25 14:49:12 +0000

    add additional message to header

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,49 @@
+// Components 
+import { useWindowWide } from '../../lib/hooks/useWindowSize'
+
+// Assets
+import reactLogo from '/react.svg'
+import viteLogo from '/vite.svg'
+
 import styles from './footer.module.css'
 
 export const Footer = () => {
+
+    const smallerHeader = !useWindowWide(780);
+
     return (
         <footer className={styles.footer}>
-            <p> © Luke Sampson Searle, 2023 </p>
+            
+            <div className={styles.div}>
+                <span className={styles.span}> © Luke Sampson Searle, 2023 </span>
+            </div>
+
+            {/* As the footer gets crowded at mobile width, this is only rendered if not mobile */}
+            { smallerHeader ? null :
+            
+            <div className={styles.div}>
+                
+                <span className={styles.span}>{" App created with React "}</span>
+                <img 
+                className={styles.div}
+                 src={reactLogo}
+                alt="React logo" 
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.react.dev", '_blank');
+                }} />
+                
+                <span className={styles.span}>{" and Vite "}</span>
+                <img 
+                className={styles.div} 
+                src={viteLogo} 
+                alt="Vite logo" 
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://vitejs.dev", '_blank');
+                }}
+                />
+            </div>}
         </footer>
     )
 }
