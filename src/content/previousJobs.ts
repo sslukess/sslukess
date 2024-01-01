dd6add43c2b60de92bba242725baa4d7edae67c9
sslukess <lvss.dev@gmail.com>
2024-01-01 06:53:44 +0000

    update jobs

--- src/content/previousJobs.ts
+++ src/content/previousJobs.ts
@@ -3,7 +3,7 @@ import { IPreviousJob } from "../components/previous-job/previousJob"
 const flgDev: IPreviousJob = {
     title: "Software Developer",
     employer: "Fitness and Lifestyle Group",
-    yearFrom: "Nov, 2022",
+    yearFrom: "Aug, 2022",
     yearTo: "Present",
     description: "After leading product requirements and planning at FLG Digital (see below), I returned to a development role, co-leading the team, using my technical skills to deliver great solutions within a Scrum team."    
 };
@@ -12,7 +12,7 @@ const flgBA: IPreviousJob = {
     title: "Lead Technical Analyst",
     employer: "Fitness and Lifestyle Group",
     yearFrom: "Dec, 2021",
-    yearTo: "Nov, 2022",
+    yearTo: "Aug, 2022",
     description: "I joined Fitness and Lifestyle Group as the lead Technical Analyst in their Digital Product team. Leveraging my technical expertise and business stakeholder collaboration skillset, I delivered top-notch digital solutions with the Scrum team."
 };
 
