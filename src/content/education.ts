69119d19a71dd452190e30bec245801874ecb276
sslukess <lvss.dev@gmail.com>
2024-01-31 12:35:08 +0000

    Fix typo in edu

--- src/content/education.ts
+++ src/content/education.ts
@@ -5,7 +5,7 @@ const uscBCS: IEducation = {
     educator: "The University of the Sunshine Coast",
     yearFrom: "2013",
     yearTo: "2017",
-    description: "During my study I focused heavily on applied and pure maths, while also gaining broader experience in the natural sciences"    
+    description: "During my study I focused heavily on applied and pure maths, while also gaining broader experience in the natural sciences."    
 };
 
 
